9e042690d276c6fb9dc72e7d8ae72eb1c63199ea
marshal <marshal.wu@gmail.com>
2013-11-06 15:11:59 +0800

    实现最简单的OpenCv调用。

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,7 +1,11 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 LOCAL_LDLIBS :=-llog
+
 OPENCV_LIB_TYPE:=STATIC 
+include /opt/OpenCV-2.4.6-android-sdk/sdk/native/jni/OpenCV.mk
+
 LOCAL_MODULE    := NativeUtils 
 LOCAL_SRC_FILES := NativeUtils.cpp
 APP_STL:=stlport_static
--- jni/NativeUtils.cpp
+++ jni/NativeUtils.cpp
@@ -2,14 +2,19 @@
 #include <stdio.h>
 #include <iostream>
 
+#include <opencv2/opencv.hpp>
 #include <android/log.h>
+
 #include "marshal_helloubuntuapp_NativeUtils.h"
 
 using namespace std;
+using namespace cv;
 
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native_utils", __VA_ARGS__))
 
 JNIEXPORT void JNICALL Java_marshal_helloubuntuapp_NativeUtils_sayHello
   (JNIEnv *env, jobject thiz){
+	FastFeatureDetector fast(40);
+	Mat a;
 	LOGI("测试一下");
 }
